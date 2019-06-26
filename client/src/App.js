e43cb492b7c3dc46688d3fe5614dd54edc25a7d3
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-06-27 00:26:09 +0700

    Complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:6969/api', {
+        responseType: 'arraybuffer',
+        headers: {
+          Accept: 'application/pdf'
+        }
+      })
+      .then(response => setData(response.data))
+      .catch(_ => setData({}));
+  }, []);
+
+  function handleClick() {
+    const file = new Blob([data], { type: 'application/pdf' });
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(file);
+    link.download = `certificate.pdf`;
+    link.click();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleClick} className="btn btn-primary rounded mt-5">
+          Export Certificate
+        </button>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
-const express = require('express');
 const fs = require('fs');
+const express = require('express');
+const cors = require('cors');
 const pdf = require('html-pdf');
 
 const app = express();
 
+app.use(cors());
+
 app.get('/', (req, res) => res.send('<h3>👋 Hello</h3>'));
 app.get('/api', (req, res) => {
   const html = fs.readFileSync('./index.html', 'utf8');
-  pdf.create(html).toBuffer(function(err, buffer){
-     res.json(buffer);
+  pdf.create(html).toBuffer(function(err, buffer) {
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Length': pdf.length
+    });
+    res.send(buffer);
   });
-})
+});
 
 app.listen(6969, () => {
   console.log(`Server started on 6969`);
